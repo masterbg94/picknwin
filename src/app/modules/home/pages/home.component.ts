13d3f7f7dc2b788e770c69519f959253e095c2f2
masterbg94 <nmilinkovic3@gmail.com>
2021-06-07 15:45:30 +0200

    - home.component.html-matches list get data fix

--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {take} from 'rxjs/operators';
+import {Matches, MatchesData} from '../../../shared/models/matches';
+import {MatchesService} from '../../../shared/services/matches.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  allMatches: MatchesData[];
+
+  constructor(private matchesService: MatchesService) {
+  }
+
+  ngOnInit(): void {
+    this.matchesService.getAllMatches().pipe(take(1)).subscribe(
+      (response: Matches) => {
+        this.allMatches = response.data;
+      }
+    );
+  }
+
+}
