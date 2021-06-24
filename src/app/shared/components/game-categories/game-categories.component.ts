0960d4a56b51e2ca17e640ad66f25d595255013d
masterbg94 <nmilinkovic3@gmail.com>
2021-06-24 19:31:55 +0200

    * Game categories collapse

--- src/app/shared/components/game-categories/game-categories.component.ts
+++ src/app/shared/components/game-categories/game-categories.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-game-categories-component',
   templateUrl: './game-categories.component.html',
   styleUrls: ['./game-categories.component.scss'],
 })
-export class GameCategoriesComponent {}
+export class GameCategoriesComponent {
+  collapsed = false;
+
+  selectCategory(x) {
+    alert(`selected category: ${x}`);
+  }
+
+  collapse() {
+    this.collapsed = !this.collapsed;
+  }
+}
--- src/app/shared/components/game-categories/game-categories.module.ts
+++ src/app/shared/components/game-categories/game-categories.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { GameCategoriesComponent } from './game-categories.component';
+import {CommonModule} from '@angular/common';
+import {SharedModule} from '../../../modules/shared.module';
 
 @NgModule({
   declarations: [GameCategoriesComponent],
-  imports: [],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
   exports: [GameCategoriesComponent],
 })
 export class GameCategoriesModule {}
