cc242a840c1dea36d6ddcc5bc3a66901080e33e7
masterbg94 <nmilinkovic3@gmail.com>
2021-07-10 19:15:42 +0200

    - responsive HOME, app side menu - home, single-match, footer, leadbord - Material sidenav with routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,45 @@ export class AppComponent implements OnDestroy {
   _mobileQueryListener: () => void;
   innerWidth: number;
 
+  sideMenu = [
+    {
+      path: '/',
+      name: 'Home',
+      icon: ''
+    },
+    {
+      path: '/matches',
+      name: 'Matches',
+      icon: ''
+    },
+    {
+      path: '/leadbord',
+      name: 'Leaderbord',
+      icon: ''
+    },
+    {
+      path: '/documents',
+      name: 'Documents',
+      icon: ''
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      icon: ''
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      icon: ''
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      icon: ''
+    }
+  ];
+
+  /** TODO: Srediti constructor izbaciti nepotrebne stvari */
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
