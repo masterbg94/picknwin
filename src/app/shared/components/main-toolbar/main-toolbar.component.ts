1ab8445cf90dc43c966a6fc341747c61c80da7a9
masterbg94 <nmilinkovic3@gmail.com>
2021-08-31 17:33:59 +0200

    Main toolbar, style -Game categories height fixed to auto - Main toolbar reformat

--- src/app/shared/components/main-toolbar/main-toolbar.component.ts
+++ src/app/shared/components/main-toolbar/main-toolbar.component.ts
@@ -1,13 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AuthenticationService} from '../../services/auth.service';
-import {Subscription} from 'rxjs';
+import {Component, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-main-toolbar-component',
   templateUrl: './main-toolbar.component.html',
   styleUrls: ['./main-toolbar.component.scss']
 })
-export class MainToolbarComponent implements OnInit {
+export class MainToolbarComponent {
   navigation = [
     {
       name: 'Home',
@@ -22,73 +21,5 @@ export class MainToolbarComponent implements OnInit {
       path: '/leadbord',
     },
   ];
-
-  /*userDropdown: any[] = [
-    {
-      name: 'Dashboard',
-      path: '/profile/',
-      icon: 'chart'
-    },
-    {
-      name: 'Notifications',
-      path: '/profile/notifications',
-      icon: 'alert'
-    },
-    {
-      name: 'Billing',
-      path: '/profile/billing',
-      icon: 'money'
-    },
-    {
-      name: 'Settings',
-      path: '/profile/settings',
-      icon: 'settings'
-    },
-    {
-      name: 'Logout',
-      path: '#',
-      icon: 'logout'
-    }
-  ];*/
-  // isDropdownToggled = false;
-  // isLogged = false;
   @Input() isLoggedUser = false;
-
-  // subscription: Subscription[] = [];
-
-  constructor(private authService: AuthenticationService) {
-  }
-
-  /*  toggleDropdown() {
-      this.isDropdownToggled = !this.isDropdownToggled;
-    }*/
-
-  /*  logoutUser() {
-      this.authService.logout();
-    }*/
-
-  ngOnInit(): void {
-    /**
-     * User login subscription
-     * implemented userLogStatus for binding in child component
-     */
-    /*    this.subscription.push(
-          this.authService.currentUser.subscribe(
-            (userLogStatus) => {
-              console.log('currentUser', userLogStatus);
-              if (userLogStatus != null) {
-                this.isLogged = true;
-                console.log('this.isLogged = true');
-              } else if (userLogStatus == null) {
-                console.log('currentUser else state', userLogStatus);
-                this.isLogged = false;
-                this.isDropdownToggled = false;
-              }
-            }, (error: any) => {
-              alert(error);
-            }
-          )
-        );*/
-  }
-
 }
