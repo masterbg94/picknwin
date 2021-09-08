6ce20bcd05e294bc963cca5cead417ad5cdbe3e8
masterbg94 <nmilinkovic3@gmail.com>
2021-09-08 17:05:55 +0200

    ios input fix (*user-select: none)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
-import { MainToolbarModule } from './shared/components/main-toolbar/main-toolbar.module';
-import { FooterModule } from './shared/components/footer/footer.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app.routing.module';
+import {MainToolbarModule} from './shared/components/main-toolbar/main-toolbar.module';
+import {FooterModule} from './shared/components/footer/footer.module';
 import {CustomMaterialModule} from './modules/custom-material.module';
 import {SharedModule} from './modules/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        MainToolbarModule,
-        FooterModule,
-        CustomMaterialModule,
-        SharedModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MainToolbarModule,
+    FooterModule,
+    CustomMaterialModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
